444e812d1dba762c6bb6f3354b382e3fc008bd79
Swastik Bose <cpswastik31@gmail.com>
2026-03-04 23:18:54 +0530

    minor changes

--- main.py
+++ main.py
@@ -27,11 +27,15 @@ def greet(
 @app.get('/products')
 def get_all_products(db : Session = Depends(get_db)) : 
     db_products = db.query(db_models.Product).all();
+    if db_products is None :
+        raise HTTPException(status_code=404, detail="No products")
     return db_products;
 
 @app.get('/products/{id}')
 def get_product_by_id(id: int, db : Session = Depends(get_db)) :
-    product = db.query(db_models.Product).filter_by(id=id).first();
+    product = db.query(db_models.Product).filter(db_models.Product.id == id).first();
+    if product is None :
+        raise HTTPException(status_code=404, detail="Product not found")
     return product;
 
 @app.post('/product/create')
@@ -53,6 +57,9 @@ def update_product(id: int, product: ProductUpdate, db : Session = Depends(get_d
 
 @app.delete('/products/{id}')
 def delete_product(id: int, db: Session = Depends(get_db)) :
+    db_product = db.query(db_models.Product).filter_by(id=id).first();
+    if db_product is None :
+        raise HTTPException(status_code=404, detail="Product not found")
     db.query(db_models.Product).filter_by(id=id).delete();
     db.commit();
     return "product deleted successfully"
